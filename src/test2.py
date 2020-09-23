b6f0185357cdef6537a08e5438108ec4de786b69
João Correia <jfscorreia@ceb.uminho.pt>
2020-09-23 09:52:27 +0100

    test files

---
+++ src/test2.py
@@ -0,0 +1,15 @@
+import deepchem as dc
+from deepchem.models.graph_models import GraphConvModel
+
+tox21_tasks, tox21_datasets, transformers = dc.molnet.load_tox21(featurizer='GraphConv', reload=False)
+train_dataset, valid_dataset, test_dataset = tox21_datasets
+
+n_tasks = len(tox21_tasks)
+model = GraphConvModel(n_tasks, batch_size=50, mode='classification')
+
+num_epochs = 10
+losses = []
+for i in range(num_epochs):
+    loss = model.fit(train_dataset, nb_epoch=1)
+    print("Epoch %d loss: %f" % (i, loss))
+    losses.append(loss)

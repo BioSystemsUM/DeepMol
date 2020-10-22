20252112015e3f9c9f30c2d6472f3b1614492d60
João Correia <jfscorreia@ceb.uminho.pt>
2020-10-22 11:06:21 +0100

    file added

---
+++ src/compoundFeaturization/morganFingerprints.py
@@ -0,0 +1,64 @@
+from baseFeaturizer import MolecularFeaturizer
+from rdkit.Chem import rdMolDescriptors
+import numpy as np
+
+
+class MorganFingerprint(MolecularFeaturizer):
+    """Morgan fingerprints.
+    Extended Connectivity Circular Fingerprints compute a bag-of-words style
+    representation of a molecule by breaking it into local neighborhoods and
+    hashing into a bit vector of the specified size.
+    """
+
+      def __init__(self,
+                   radius: int = 2,
+                   size: int = 1024,
+                   chiral: bool = False,
+                   bonds: bool = True,
+                   features: bool = False):
+        """
+        Parameters
+        ----------
+        radius: int, optional (default 2)
+          Fingerprint radius.
+        size: int, optional (default 1024)
+          Length of generated bit vector.
+        chiral: bool, optional (default False)
+          Whether to consider chirality in fingerprint generation.
+        bonds: bool, optional (default True)
+          Whether to consider bond order in fingerprint generation.
+        features: bool, optional (default False)
+          Whether to use feature information instead of atom information;
+        """
+
+        self.radius = radius
+        self.size = size
+        self.chiral = chiral
+        self.bonds = bonds
+        self.features = features
+
+      def _featurize(self, mol: RDKitMol) -> np.ndarray:
+        """Calculate circular fingerprint.
+        Parameters
+        ----------
+        mol: rdkit.Chem.rdchem.Mol
+          RDKit Mol object
+        Returns
+        -------
+        np.ndarray
+          A numpy array of circular fingerprint.
+        """
+
+        try :
+            fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(mol,
+                                                                self.radius,
+                                                                nBits=self.size,
+                                                                useChirality=self.chiral,
+                                                                useBondTypes=self.bonds,
+                                                                useFeatures=self.features)
+        except Exception as e:
+            print('error in smile: ' + str(mol))
+            fp = np.nan
+        fp = np.asarray(fp, dtype=np.float)
+
+        return fp

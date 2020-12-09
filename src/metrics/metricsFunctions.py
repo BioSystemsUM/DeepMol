47a477130619e7cb7b564de2c7ec2747426ad32b
João Correia <jfscorreia@ceb.uminho.pt>
2020-12-09 10:36:55 +0000

    new metrics imported

--- src/metrics/metricsFunctions.py
+++ src/metrics/metricsFunctions.py
@@ -2,5 +2,32 @@
 Script containing imports of metrics and new metric functions.
 '''
 
-from sklearn.metrics import roc_auc_score
+
+############################################################
+# CLASSIFICATION
+############################################################
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import brier_score_loss
+from sklearn.metrics import f1_score
+from sklearn.metrics import log_loss
 from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import roc_auc_score
+
+############################################################
+# REGRESSION
+############################################################
+
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import max_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_log_error
+from sklearn.metrics import median_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_poissom_deviance
+from sklearn.metrics import mean_gamma_deviance

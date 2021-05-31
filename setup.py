25abd10f6f5670d469c0a1b33251cd861c9f7e2d
João Correia <jfscorreia@ceb.uminho.pt>
2021-05-31 16:20:54 +0100

    [ADD] setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='src',
+      version='1.0',
+      packages=find_packages(),
+      author='BiSBII CEB University of Minho',
+      author_email='jfscorreia95@gmail.com',
+      description='DeepMol: ...',
+      license='BSD-2-Clause License',
+      keywords='machine-learning deep-learning chemoinformatics',
+      url=''
+      )

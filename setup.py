d26d55c4a9ac35e2cfb2ce7a042014a5669ebb09
jcapels <jcapels96@gmail.com>
2022-10-10 15:01:09 +0100

    [FIX] install_requirements at setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(install_requires=[
+        "rdkit-pypi==2022.3.5",
+        "smilespe==0.0.3",
+        "seaborn==0.12.0",
+        "joblib==1.1.0",
+        "pillow==8.4.0",
+        "h5py==3.1.0",
+        "shap==0.41.0"
+    ])

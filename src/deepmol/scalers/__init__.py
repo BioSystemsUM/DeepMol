f7decafc0b05eca1f210b0a37e24894bc4ae2345
João Correia <jfscorreia95@gmail.com>
2022-11-15 13:49:46 +0000

    [UPDATE] docstrings, typehints and code formatting

--- src/deepmol/scalers/__init__.py
+++ src/deepmol/scalers/__init__.py
@@ -1,2 +1,2 @@
 from .sklearn_scalers import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler, PolynomialFeatures, Normalizer, \
-    Binarizer, KernelCenterer, QuantileTransformer, PowerTransformer
+    Binarizer, KernelCenterer, QuantileTransformer, PowerTransformer, BaseScaler

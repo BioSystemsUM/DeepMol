4202fb6e72c92401b401dabe2d6dc9a37968d722
João Correia <jfscorreia95@gmail.com>
2023-02-15 16:52:37 +0000

    [ADD] more tests for the NumpyDataset Class (now coverage is 100%)

--- tests/unit_tests/dataset/test_dataset.py
+++ tests/unit_tests/dataset/test_dataset.py
@@ -1,101 +1,229 @@
 import os
+import shutil
 from unittest import TestCase
 
-from deepmol.compound_featurization import MorganFingerprint
-from deepmol.loaders.loaders import CSVLoader
+import numpy as np
 import pandas as pd
 
-from tests import TEST_DIR
+from deepmol.datasets import NumpyDataset
 
 
 class TestDataset(TestCase):
 
     def setUp(self) -> None:
-        dataset = os.path.join(TEST_DIR, "data", "test_to_convert_to_sdf.csv")
-        loader = CSVLoader(dataset,
-                           mols_field='Standardized_Smiles',
-                           labels_fields='Class')
+        self.mols = ['C', 'CC', 'CCC']
+        self.base_dataset = NumpyDataset(mols=self.mols)
+        self.output_dir = 'outputs/'
+        if not os.path.exists(self.output_dir):
+            os.mkdir(self.output_dir)
 
-        self.dataset_to_test = loader.create_dataset()
-        MorganFingerprint().featurize(self.dataset_to_test)
+    def tearDown(self):
+        if os.path.exists(self.output_dir):
+            shutil.rmtree(self.output_dir)
 
-    def test_merge_datasets(self):
-        dataset = os.path.join(TEST_DIR, "data", "train_dataset.csv")
-        pandas_dset = pd.read_csv(dataset)
-        columns = list(pandas_dset.columns[3:])
+    def test_numpy_dataset_args(self):
+        for i, mol in enumerate(self.mols):
+            self.assertIn(mol, self.base_dataset.mols[i])
+        self.assertEqual(self.base_dataset.__len__(), 3)
 
-        loader = CSVLoader(dataset,
-                           features_fields=columns,
-                           mols_field='mols',
-                           labels_fields='y')
-        dataset = loader.create_dataset()
+        self.base_dataset.mols = ['C', 'CC', 'CCC', 'CCCC']
+        self.assertEqual(self.base_dataset.__len__(), 4)
 
-        self.dataset_to_test.merge([dataset])
+        with self.assertRaises(ValueError):
+            self.base_dataset.y = [1, 0, 1]
+        self.base_dataset.y = [1, 0, 0, 1]
 
-    def test_load_dataset_with_features(self):
-        dataset = os.path.join(TEST_DIR, "data", "train_dataset.csv")
-        pandas_dset = pd.read_csv(dataset)
-        columns = list(pandas_dset.columns[3:])
+        n_mols, n_features, n_y = self.base_dataset.get_shape()
+        self.assertEqual(n_mols, (4,))
+        self.assertEqual(n_y, (4,))
+        self.assertIsNone(n_features)
 
-        loader = CSVLoader(dataset,
-                           features_fields=columns,
-                           mols_field='mols',
-                           labels_fields='y')
+        self.base_dataset.n_tasks = 1
+        self.assertEqual(self.base_dataset.n_tasks, 1)
 
-        dataset = loader.create_dataset()
-        self.assertEqual(len(dataset.features2keep), len(columns))
+        self.base_dataset.X = []
+        self.assertIsNone(self.base_dataset.X)
 
-    def test_select_rows(self):
-        self.dataset_to_test.select([0, 2], axis=0)
-
-        self.assertEqual(self.dataset_to_test.X.shape[0], 2)
-
-    def test_multiple_select_rows(self):
-        self.dataset_to_test.select([0, 2, 3, 4], axis=0)
-        self.assertEqual(self.dataset_to_test.X.shape[0], 4)
-
-        self.dataset_to_test.select([2, 3], axis=0)
-        self.assertEqual(self.dataset_to_test.X.shape[0], 2)
-
-        self.dataset_to_test.select([2], axis=0)
-        self.assertEqual(self.dataset_to_test.X.shape[0], 1)
-
-    def test_select_columns(self):
-        self.dataset_to_test.select([i for i in range(6)], axis=1)
-        self.assertEqual(self.dataset_to_test.X.shape[1], 6)
-
-    def test_sequential_multiselect_select_columns(self):
-        self.dataset_to_test.select([i for i in range(6)], axis=1)
-        self.assertEqual(self.dataset_to_test.X.shape[1], 6)
-
-        self.dataset_to_test.select([i for i in range(3)], axis=1)
-        self.assertEqual(self.dataset_to_test.X.shape[1], 3)
-
-        self.dataset_to_test.select([i for i in range(1)], axis=1)
-        self.assertEqual(self.dataset_to_test.X.shape[1], 1)
-
-    def test_random_multiselect_select_columns(self):
-        self.dataset_to_test.select([i for i in range(100)], axis=1)
-        self.assertEqual(self.dataset_to_test.X.shape[1], 100)
-
-        self.dataset_to_test.select([4, 60, 40, 20, 39], axis=1)
-        self.assertEqual(self.dataset_to_test.X.shape[1], 5)
-
-        self.dataset_to_test.select([4, 60, 40], axis=1)
-        self.assertEqual(self.dataset_to_test.X.shape[1], 3)
-
-        self.assertEqual(set(self.dataset_to_test.features2keep), {4, 60, 40})
-
-    def test_select_features(self):
-        self.dataset_to_test.select_features([i for i in range(100)])
-        self.assertEqual(self.dataset_to_test.X.shape[1], 100)
-
-        self.dataset_to_test.select_features([4, 60, 40, 20, 39])
-        self.assertEqual(self.dataset_to_test.X.shape[1], 5)
+    def test_remove_duplicates(self):
+        smiles = ['C', 'CC', 'CCC']
+        features = [[1, 0, 1], [0, 1, 0], [1, 0, 1]]
+        y = [1, 0, 1]
+        ids = [1, 2, 3]
+        dataset = NumpyDataset(mols=smiles, X=features, y=y, ids=ids, features2keep=[0, 1, 2], n_tasks=1)
+        dataset.remove_duplicates()
+        self.assertEqual(len(dataset), 2)
 
     def test_remove_elements(self):
-        self.dataset_to_test.remove_elements([0, 1, 3])
-        self.assertEqual(self.dataset_to_test.X.shape[0], 2)
+        smiles = ['C', 'CC', 'CCC', 'CCCC', 'CCCCC']
+        ids = [1, 2, 3, 'four', 'five']
+        dataset = NumpyDataset(mols=smiles, ids=ids)
+        dataset.remove_elements([1, 'five'])
+        self.assertEqual(len(dataset), 3)
 
-        self.dataset_to_test.remove_elements([2])
-        self.assertEqual(self.dataset_to_test.X.shape[0], 1)
+        dataset.remove_elements(['five'])
+        self.assertEqual(len(dataset), 3)
+
+        dataset.remove_elements([2, 3])
+        self.assertEqual(len(dataset), 1)
+
+        dataset.remove_elements(['four'])
+        self.assertEqual(len(dataset), 0)
+
+    def test_select_features(self):
+        smiles = ['C', 'CC', 'CCC']
+        features = [[1, 0, 1], [0, 1, 0], [1, 0, 1]]
+        dataset = NumpyDataset(mols=smiles, X=features)
+        dataset.select_features([0, 2])
+        self.assertEqual(dataset.X.shape[1], 2)
+        self.assertEqual(len(dataset.X[0]), 2)
+        self.assertEqual(len(dataset.X[1]), 2)
+        self.assertEqual(len(dataset.X[2]), 2)
+
+        dataset.select_features(['a', 'c'])
+        self.assertEqual(dataset.X.shape[1], 0)
+
+    def test_remove_nan(self):
+        smiles = ['C', 'CC', 'CCC', 'CCCC', 'CCCCC']
+        features = [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 0.0, 1.0], [1.0, 0.0, 1.0], [1.0, 0.0, 1.0]]
+        dataset = NumpyDataset(mols=smiles, X=features)
+        dataset.remove_nan()
+        dataset.remove_nan(axis=1)
+        self.assertEqual(len(dataset), 5)
+
+        dataset.X = [[np.nan, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 0.0, 1.0], [1.0, 0.0, 1.0], [1.0, 0.0, 1.0]]
+        dataset.remove_nan()
+        dataset.remove_nan(axis=1)
+        self.assertEqual(len(dataset), 4)
+        self.assertEqual(len(dataset.ids), 4)
+
+        dataset.X = [[np.nan, 1.0, np.nan],
+                     [np.nan, 1.0, 0.0],
+                     [np.nan, 0.0, 1.0],
+                     [np.nan, 0.0, 1.0],
+                     [np.nan, 0.0, 1.0]]
+        dataset.ids = [1, 2, 3, 4, 5]
+        dataset.remove_nan(axis=1)
+        self.assertEqual(len(dataset), 4)
+        self.assertEqual(len(dataset.ids), 5)
+        self.assertEqual(dataset.X.shape[1], 1)
+        dataset.remove_nan()
+        self.assertEqual(len(dataset), 4)
+
+        dataset.mols = ['C', 'CC', 'CCC']
+        dataset.X = [np.nan, 1.0, np.nan]
+        dataset.ids = [1, 2, 3]
+        dataset.remove_nan()
+        self.assertEqual(len(dataset), 1)
+        self.assertEqual(dataset.get_shape()[0], (1,))
+        self.assertEqual(dataset.get_shape()[1], (1,))
+        self.assertIsNone(dataset.get_shape()[2])
+
+    def test_select_to_split(self):
+        dataset = NumpyDataset(mols=['C', 'CC', 'CCC', 'CCCC', 'CCCCC'],
+                               X=[[1, 0, 1], [0, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1]],
+                               y=[1, 0, 1, 0, 1],
+                               ids=[1, 2, 3, 4, 5],
+                               features2keep=[0, 1, 2],
+                               n_tasks=1)
+        split = dataset.select_to_split([1, 2, 3])
+        self.assertEqual(len(split), 3)
+        self.assertEqual(len(split.mols), 3)
+        self.assertEqual(split.X.shape, (3, 3))
+        self.assertEqual(len(split.y), 3)
+        self.assertEqual(len(split.ids), 3)
+        self.assertEqual(split.n_tasks, 1)
+
+        dataset2 = NumpyDataset(mols=['C', 'CC', 'CCC', 'CCCC', 'CCCCC'],
+                                X=[1, 0, 1, 1, 1],
+                                y=[1, 0, 1, 0, 1],
+                                ids=[1, 2, 3, 4, 5],
+                                n_tasks=1)
+        split2 = dataset2.select_to_split([1, 2, 3])
+        self.assertEqual(len(split2), 3)
+        self.assertEqual(len(split2.mols), 3)
+        self.assertEqual(split2.X.shape, (3,))
+        self.assertEqual(len(split2.y), 3)
+        self.assertEqual(len(split2.ids), 3)
+        self.assertEqual(split2.n_tasks, 1)
+
+    def test_merge(self):
+        d1 = NumpyDataset(mols=['CCCCCCCCCC', 'CCCCCCCCCCCCCCC'],
+                          X=[[1, 0, 1], [0, 1, 0]],
+                          y=[1, 0],
+                          ids=[3, 4])
+        d2 = NumpyDataset(mols=['CCCCCCCC', 'CCCCCCCCCCCCC'],
+                          X=[[1, 0, 1], [0, 1, 0]],
+                          y=[1, 0],
+                          ids=[5, 6])
+
+        m = self.base_dataset.merge([d1, d2])
+        self.assertIsNone(m.X)
+        self.assertEqual(len(m), 7)
+        self.assertEqual(len(m.ids), 7)
+        self.assertEqual(len(m.y), 7)
+
+        d = NumpyDataset(mols=['CCCCCCCC', 'CCCCCCCCCCCCC', 'c'],
+                         X=[[1, 0, 1], [0, 1, 0], [1, 0, 1]],
+                         y=[1, 0, 1],
+                         ids=[5, 6, 7])
+        with self.assertRaises(ValueError):
+            d.merge([d1, d2])
+        d.ids = [10, 20, 30]
+        merged = d.merge([d1, d2])
+        self.assertEqual(len(merged), 7)
+        self.assertEqual(len(merged), len(d) + len(d1) + len(d2))
+        self.assertEqual(len(merged.mols), 7)
+        self.assertEqual(len(merged.mols), len(d.mols) + len(d1.mols) + len(d2.mols))
+        self.assertEqual(merged.X.shape, (7, 3))
+        self.assertEqual(merged.X.shape, (d.X.shape[0] + d1.X.shape[0] + d2.X.shape[0], d.X.shape[1]))
+
+        d3 = NumpyDataset(mols=['CCCCCCCC', 'CCCCCCCCCCCCC', 'c'],
+                          X=[1, 0, 1],
+                          y=[1, 0, 1],
+                          ids=[5, 6, 7])
+        d4 = NumpyDataset(mols=['CCCCCCCCCC', 'CCCCCCCCCCCCCCC'],
+                          X=[1, 0])
+        merged2 = d3.merge([d4])
+        self.assertEqual(len(merged2), 5)
+
+        dd = NumpyDataset(mols=['CCCCCCCC', 'CCCCCCCCCCCCC', 'c'],
+                          X=[[1, 1], [0, 0], [1, 1]],
+                          y=[1, 0, 1],
+                          ids=[50, 60, 70])
+
+        merged_x = dd.merge([d4])
+        self.assertIsNone(merged_x.X)
+        merged_x2 = dd.merge([d])
+        self.assertIsNone(merged_x2.X)
+
+    def test_save_to_csv(self):
+        df = NumpyDataset(mols=['C', 'CC', 'CCC', 'CCCC', 'CCCCC'],
+                          X=[[1, 0, 1], [0, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1]],
+                          y=[1, 0, 1, 0, 1],
+                          ids=[1, 2, 3, 4, 5],
+                          features2keep=[0, 1, 2],
+                          n_tasks=1)
+        df.save_to_csv(os.path.join(self.output_dir, 'test.csv'))
+        pd_df = pd.read_csv(os.path.join(self.output_dir, 'test.csv'))
+        # ids + mols + y + features
+        self.assertEqual(pd_df.shape, (len(df.mols), 3 + df.X.shape[1]))
+
+    def test_save_load_features(self):
+        d = NumpyDataset(mols=['C', 'CC', 'CCC', 'CCCC', 'CCCCC'])
+        with self.assertRaises(ValueError):
+            d.save_features(os.path.join(self.output_dir, 'test.csv'))
+
+        d1 = NumpyDataset(mols=['CCCCCCCCCC', 'CCCCCCCCCCCCCCC'],
+                            X=[[1, 0, 1], [0, 1, 0]],
+                            y=[1, 0],
+                            ids=[3, 4])
+        d1.save_features(os.path.join(self.output_dir, 'test.csv'))
+
+        d2 = NumpyDataset(mols=['CCCCCCCCCC', 'CCCCCCCCCCCCCCC'])
+        d2.load_features(os.path.join(self.output_dir, 'test.csv'))
+
+        self.assertEqual(d1.X.shape, d2.X.shape)
+        for i in range(d1.X.shape[0]):
+            for j in range(d1.X.shape[1]):
+                self.assertEqual(d1.X[i, j], d2.X[i, j])
+

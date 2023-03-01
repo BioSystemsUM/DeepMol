f0786a16f74d6c0f6b5efe9fcf5b40c6caa36100
João Correia <jfscorreia95@gmail.com>
2023-03-01 19:51:58 +0000

    [FIX] test according to recent changes

--- tests/unit_tests/featurizers/test_featurizers.py
+++ tests/unit_tests/featurizers/test_featurizers.py
@@ -5,6 +5,7 @@ from unittest.mock import MagicMock
 
 import numpy as np
 import pandas as pd
+from rdkit.Chem import MolFromSmiles
 
 from deepmol.datasets import SmilesDataset
 from deepmol.loaders.loaders import CSVLoader
@@ -37,13 +38,24 @@ class FeaturizerTestCase(ABC):
 
         data_path = os.path.join(TEST_DIR, 'data/test_to_convert_to_sdf.csv')
         self.original_smiles = pd.read_csv(data_path, sep=',').Smiles.values
+        mols = [self._smiles_to_mol(s) for s in self.original_smiles]
         self.mock_dataset = MagicMock(spec=SmilesDataset,
-                                      mols=self.original_smiles,
+                                      smiles=self.original_smiles,
+                                      mols=mols,
                                       ids=np.arange(len(self.original_smiles)))
         self.original_smiles_with_invalid = np.append(self.original_smiles, ['CC(=O)[O-].NC', 'C1=CC=CC=C1('])
+        mols = [self._smiles_to_mol(s) for s in self.original_smiles_with_invalid]
         self.mock_dataset_with_invalid = MagicMock(spec=SmilesDataset,
-                                                   mols=self.original_smiles_with_invalid,
+                                                   smiles=self.original_smiles_with_invalid,
+                                                   mols=mols,
                                                    ids=np.arange(len(self.original_smiles_with_invalid)))
+
+    @staticmethod
+    def _smiles_to_mol(smiles):
+        try:
+            return MolFromSmiles(smiles)
+        except:
+            return None
 
     @abstractmethod
     def test_featurize(self):

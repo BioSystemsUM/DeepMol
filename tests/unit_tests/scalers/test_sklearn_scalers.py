8d5de28066d90644cddcf7d4038f114ad37fbb65
João Correia <jfscorreia95@gmail.com>
2023-03-03 17:21:39 +0000

    [ADD] tests for scalers

---
+++ tests/unit_tests/scalers/test_sklearn_scalers.py
@@ -0,0 +1,37 @@
+import copy
+from unittest import TestCase, skip
+
+import numpy as np
+
+from deepmol.scalers import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler, PolynomialFeatures, Normalizer, \
+    Binarizer, KernelCenterer, QuantileTransformer, PowerTransformer
+from unit_tests.scalers.test_scalers import TestScalers
+
+
+class TestSklearnScalers(TestScalers, TestCase):
+
+    @staticmethod
+    def get_scalers():
+        return [StandardScaler(), MinMaxScaler(), MaxAbsScaler(), RobustScaler(), Normalizer(),
+                Binarizer(), KernelCenterer(), QuantileTransformer(), PowerTransformer()]
+
+    def test_scaler(self):
+        for scaler in self.get_scalers():
+            df = copy.deepcopy(self.dataset)
+            df2 = copy.deepcopy(self.dataset)
+            scaler.fit_transform(df)
+            # assert data has changed
+            self.assertFalse((self.dataset.X == df.X).all())
+
+            scaler.save("test_scaler.pkl")
+            scaler.load("test_scaler.pkl")
+
+            scaler.fit_transform(df2)
+            self.assertTrue((df.X == df2.X).all())
+
+    @skip("Not implemented. This scaler changes the number of features!")
+    def test_polynomial_features(self):
+        df = copy.deepcopy(self.polynomial_features)
+        scaler = PolynomialFeatures(degree=2)
+        scaler.fit_transform(df)
+        self.assertFalse((self.polynomial_features.X == df.X).all())

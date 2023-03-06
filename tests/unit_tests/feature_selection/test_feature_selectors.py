82376674df945bb0994b19868fb04e36dd26647b
João Correia <jfscorreia95@gmail.com>
2023-03-06 12:51:59 +0000

    [ADD] simple unit tests for all feature selectors

--- tests/unit_tests/feature_selection/test_feature_selectors.py
+++ tests/unit_tests/feature_selection/test_feature_selectors.py
@@ -1,9 +1,13 @@
+import copy
 import os
 from unittest import TestCase
+from unittest.mock import MagicMock
 
-from deepmol.compound_featurization.rdkit_descriptors import TwoDimensionDescriptors
-from deepmol.feature_selection import BorutaAlgorithm
-from deepmol.loaders.loaders import CSVLoader
+import numpy as np
+import pandas as pd
+
+from deepmol.datasets import SmilesDataset
+from deepmol.feature_selection import BorutaAlgorithm, LowVarianceFS, KbestFS, PercentilFS, RFECVFS, SelectFromModelFS
 
 from tests import TEST_DIR
 
@@ -11,25 +15,39 @@ from tests import TEST_DIR
 class TestFeatureSelectors(TestCase):
 
     def setUp(self) -> None:
-        dataset = os.path.join(TEST_DIR, "data", "test_to_convert_to_sdf.csv")
-        loader = CSVLoader(dataset,
-                           smiles_field='Standardized_Smiles',
-                           labels_fields=['Class'])
-
-        self.mini_dataset_to_test = loader.create_dataset()
+        dataset = os.path.join(TEST_DIR, "data", "train_dataset.csv")
+        td = pd.read_csv(dataset, sep=',', nrows=250)
+        y = td.y.values
+        feature_names = td.columns.values[3:]
+        x = td.loc[:, feature_names].values
+        # add some random values between 0 and 5 to the features in the first 10 columns
+        x[:, :10] += np.random.randint(0, 5, size=(x.shape[0], 10))
+        self.features_dataset = MagicMock(spec=SmilesDataset,
+                                          X=x,
+                                          y=y,
+                                          feature_names=feature_names,
+                                          mode='classification')
 
     def tearDown(self) -> None:
         if os.path.exists('deepmol.log'):
             os.remove('deepmol.log')
 
-    def test_boruta_algorithm_larger_dataset(self):
-        # TODO: verify boruta is working as expected
-        import pandas as pd
-        dataset = os.path.join(TEST_DIR, "data", "test_to_convert_to_sdf.csv")
-        pandas_dset = pd.read_csv(dataset)
-        columns = list(pandas_dset.columns[3:])
-        TwoDimensionDescriptors().featurize(self.mini_dataset_to_test)
-        BorutaAlgorithm(max_iter=3, n_estimators=50, support_weak=True).select_features(self.mini_dataset_to_test)
-        # 208 features created by the TwoDimensionDescriptors featurizer
-        self.assertLessEqual(208, self.mini_dataset_to_test.X.shape[1])
+    def validate_feature_selector(self, feature_selector, **kwargs):
+        df = copy.deepcopy(self.features_dataset)
 
+        def side_effect(arg):
+            df.X = df.X[:, arg]
+            df.feature_names = df.feature_names[arg]
+
+        self.features_dataset.select_features_by_index.side_effect = side_effect
+        feature_selector(**kwargs).select_features(df)
+        self.assertLessEqual(len(self.features_dataset.feature_names), len(df.feature_names))
+        self.assertLessEqual(self.features_dataset.X.shape[1], df.X.shape[1])
+
+    def test_feature_selectors(self):
+        self.validate_feature_selector(LowVarianceFS, threshold=0.99)
+        self.validate_feature_selector(KbestFS, k=10)
+        self.validate_feature_selector(PercentilFS, percentil=10)
+        self.validate_feature_selector(RFECVFS, step=10)
+        self.validate_feature_selector(SelectFromModelFS, threshold=0.1)
+        self.validate_feature_selector(BorutaAlgorithm, max_iter=3, n_estimators=50, support_weak=True)

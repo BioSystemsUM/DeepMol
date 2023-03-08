3b6edeb29c57f81717ea7400feade9306971fadf
João Correia <jfscorreia95@gmail.com>
2023-03-08 13:24:14 +0000

    [FIX] when n_components is higher than 3 the method should be 'exact'

--- tests/unit_tests/unsupervised/test_tsne.py
+++ tests/unit_tests/unsupervised/test_tsne.py
@@ -7,16 +7,16 @@ from unit_tests.unsupervised.test_unsupervised import UnsupervisedBaseTestCase
 
 class TestTSNE(UnsupervisedBaseTestCase, TestCase):
 
-    def validate_tsne_classification(self, n_components):
+    def validate_tsne_classification(self, n_components, **kwargs):
         dataset = copy(self.dataset)
-        pca = TSNE(n_components=n_components)
+        pca = TSNE(n_components=n_components, **kwargs)
         components_df = pca.run_unsupervised(dataset)
         self.assertEqual(components_df.X.shape, (dataset.X.shape[0], n_components))
         pca.plot(components_df.X, path='test_components.png')
 
-    def validate_tsne_regression(self, n_components):
+    def validate_tsne_regression(self, n_components, **kwargs):
         dataset = copy(self.regression_dataset)
-        pca = TSNE(n_components=n_components)
+        pca = TSNE(n_components=n_components, **kwargs)
         components_df = pca.run_unsupervised(dataset)
         self.assertEqual(components_df.X.shape, (dataset.X.shape[0], n_components))
         pca.plot(components_df.X, path='test_components.png')
@@ -24,8 +24,8 @@ class TestTSNE(UnsupervisedBaseTestCase, TestCase):
     def test_run_unsupervised(self):
         self.validate_tsne_classification(2)
         self.validate_tsne_classification(3)
-        self.validate_tsne_classification(4)
+        self.validate_tsne_classification(4, method='exact')
 
         self.validate_tsne_regression(2)
         self.validate_tsne_regression(3)
-        self.validate_tsne_regression(4)
+        self.validate_tsne_regression(4, method='exact')

fab028c9ffb9d9a1beb8bc50d9e94c00d646800b
João Correia <jfscorreia95@gmail.com>
2023-03-10 15:29:07 +0000

    [REMOVE] unused arguments and simplify metric creation

--- src/deepmol/metrics/metrics.py
+++ src/deepmol/metrics/metrics.py
@@ -1,9 +1,8 @@
-from typing import Tuple, Any, Union, List
+from typing import Tuple, Any, List
 
 import numpy as np
 
 from deepmol.loggers import Logger
-from deepmol.utils.utils import normalize_labels_shape
 
 
 class Metric(object):
@@ -17,12 +16,7 @@ class Metric(object):
                  metric: callable,
                  task_averager: callable = None,
                  name: str = None,
-                 mode: str = None,
-                 n_tasks: int = None,
-                 classification_handling_mode: str = None,
-                 threshold_value: float = None,
                  **kwargs) -> None:
-        # TODO: review threshold values and change to deal with a variable threshold (i.e. different from 0.5)
         """
         Parameters
         ----------
@@ -32,23 +26,6 @@ class Metric(object):
             If not None, should be a function that averages metrics across tasks.
         name: str
             Name of this metric
-        mode: str
-            Should be "classification" or "regression."
-        n_tasks: int
-            The number of tasks this class is expected to handle.
-        classification_handling_mode: str
-            Models by default predict class probabilities for classification problems. This means that for a given
-            singletask prediction, after shape normalization, the prediction will be a numpy array of shape
-            `(N, n_classes)` with class probabilities.
-            It can take on the following values:
-            - None: default value. Pass in `y_pred` directy into the metric.
-            - "threshold": Use `threshold_predictions` to threshold `y_pred`. Use `threshold_value` as the desired
-            threshold.
-            - "threshold-one-hot": Use `threshold_predictions` to threshold `y_pred` using `threshold_values`, then
-            apply `to_one_hot` to output.
-        threshold_value: float
-            If set, and `classification_handling_mode` is "threshold" or "threshold-one-hot" apply a thresholding
-            operation to values with this threshold.
         kwargs:
             Additional arguments to pass to metric.
         """
@@ -60,66 +37,11 @@ class Metric(object):
 
         if name is None:
             if task_averager is None:
-                if hasattr(self.metric, '__name__'):
-                    self.name = self.metric.__name__
-                else:
-                    self.name = "unknown metric"
+                self.name = self.metric.__name__
             else:
-                if hasattr(self.metric, '__name__'):
-                    self.name = task_averager.__name__ + "-" + self.metric.__name__
-                else:
-                    self.name = "unknown metric"
+                self.name = self.task_averager.__name__ + "-" + self.metric.__name__
         else:
             self.name = name
-        if mode is None:
-            # Some default metrics
-            if self.metric.__name__ in ["roc_auc_score",
-                                        "matthews_corrcoef",
-                                        "recall_score",
-                                        "accuracy_score",
-                                        "kappa_score",
-                                        "cohen_kappa_score",
-                                        "precision_score",
-                                        "balanced_accuracy_score",
-                                        "prc_auc_score",
-                                        "f1_score",
-                                        "bedroc_score",
-                                        "jaccard_score",
-                                        "jaccard_index",
-                                        "pixel_error",
-                                        "confusion_matrix",
-                                        "classification_report"]:
-                mode = "classification"
-                # Defaults sklearn's metrics with required behavior
-                if classification_handling_mode is None:
-                    if self.metric.__name__ in ["matthews_corrcoef", "cohen_kappa_score", "kappa_score",
-                                                "balanced_accuracy_score", "recall_score", "jaccard_score",
-                                                "jaccard_index", "pixel_error", "f1_score"]:
-
-                        classification_handling_mode = "threshold"
-
-                    elif self.metric.__name__ in ["accuracy_score", "precision_score", "bedroc_score"]:
-                        classification_handling_mode = "threshold-one-hot"
-
-                    elif self.metric.__name__ in ["roc_auc_score", "prc_auc_score"]:
-                        classification_handling_mode = None
-
-            elif self.metric.__name__ in ["pearson_r2_score", "r2_score", "mean_squared_error",
-                                          "mean_absolute_error", "rms_score", "mae_score", "pearsonr",
-                                          "median_absolute_error"]:
-                mode = "regression"
-            else:
-                raise ValueError(
-                    "Please specify the mode of this metric. mode must be 'regression' or 'classification'")
-
-        self.mode = mode
-        self.n_tasks = n_tasks
-
-        if classification_handling_mode not in [None, "threshold", "threshold-one-hot"]:
-            raise ValueError("classification_handling_mode must be one of None, 'threshold', 'threshold_one_hot'")
-
-        self.classification_handling_mode = classification_handling_mode
-        self.threshold_value = threshold_value
         self.kwargs = kwargs
         self.logger = Logger()
 

d3a75a3863336d33c72baa6d009ac4f74220ba6c
João Correia <jfscorreia95@gmail.com>
2023-03-10 11:06:37 +0000

    [REFACTOR] use kwargs for unsupervised learn methods

--- src/deepmol/unsupervised/umap.py
+++ src/deepmol/unsupervised/umap.py
@@ -14,66 +14,36 @@ class UMAP(UnsupervisedLearn):
     (https://github.com/lmcinnes/umap)
     """
 
-    def __init__(self,
-                 parametric: bool = False,
-                 n_neighbors: int = 15,
-                 n_components: int = 2,
-                 metric: str = 'euclidean',
-                 n_epochs: int = None,
-                 learning_rate: float = 1.0,
-                 low_memory: bool = True,
-                 random_state: int = None,
-                 **kwargs):
+    def __init__(self, parametric: bool = True, **kwargs):
         """
         Initialize UMAP.
 
         Parameters
         ----------
         parametric : bool
-            If True, use parametric UMAP.
-        n_neighbors : int
-            The size of local neighborhood.
-        n_components : int
-            The dimension of the space to embed into.
-        metric : str
-            The metric to use for the computation.
-        n_epochs : int
-            The number of training epochs to use when optimizing the low dimensional embedding.
-        learning_rate : float
-            The initial learning rate for the embedding optimization.
-        low_memory : bool
-            If True, use a more memory efficient nearest neighbor implementation.
-        random_state : int
-            The random seed to use.
+                If True, use parametric UMAP.
         kwargs:
-            Additional keyword arguments for the UMAP class.
+            Additional keyword arguments for the UMAP class (see https://github.com/lmcinnes/umap). Includes:
+            n_neighbors : int
+                The size of local neighborhood.
+            n_components : int
+                The dimension of the space to embed into.
+            metric : str
+                The metric to use for the computation.
+            n_epochs : int
+                The number of training epochs to use when optimizing the low dimensional embedding.
+            learning_rate : float
+                The initial learning rate for the embedding optimization.
+            low_memory : bool
+                If True, use a more memory efficient nearest neighbor implementation.
+            random_state : int
+                The random seed to use.
         """
         super().__init__()
-        self.n_neighbors = n_neighbors
-        self.n_components = n_components
-        self.metric = metric
-        self.n_epochs = n_epochs
-        self.learning_rate = learning_rate
-        self.low_memory = low_memory
-        self.random_state = random_state
         if parametric:
-            self.umap = umap.parametric_umap.ParametricUMAP(n_neighbors=self.n_neighbors,
-                                                            n_components=self.n_components,
-                                                            metric=self.metric,
-                                                            n_epochs=self.n_epochs,
-                                                            learning_rate=self.learning_rate,
-                                                            low_memory=self.low_memory,
-                                                            random_state=self.random_state,
-                                                            **kwargs)
+            self.umap = umap.parametric_umap.ParametricUMAP(**kwargs)
         else:
-            self.umap = umap.UMAP(n_neighbors=self.n_neighbors,
-                                  n_components=self.n_components,
-                                  metric=self.metric,
-                                  n_epochs=self.n_epochs,
-                                  learning_rate=self.learning_rate,
-                                  low_memory=self.low_memory,
-                                  random_state=self.random_state,
-                                  **kwargs)
+            self.umap = umap.UMAP(**kwargs)
 
     def _run_unsupervised(self, dataset: Dataset, **kwargs) -> SmilesDataset:
         """
@@ -116,7 +86,7 @@ class UMAP(UnsupervisedLearn):
         kwargs:
             Additional keyword arguments for the plot.
         """
-        self.logger.info(f'{self.n_components} Components UMAP: ')
+        self.logger.info(f'{x_new.shape[1]} Components UMAP: ')
 
         if self.dataset.mode == 'classification':
             y = [str(i) for i in self.dataset.y]
@@ -130,11 +100,11 @@ class UMAP(UnsupervisedLearn):
             fig = px.scatter_3d(x_new, x=0, y=1, z=2, color=y,
                                 labels={'0': 'PC 1', '1': 'PC 2', '2': 'PC 3', 'color': self.dataset.label_names[0]})
         else:
-            labels = {str(i): f"UMAP {i + 1}" for i in range(self.n_components)}
+            labels = {str(i): f"UMAP {i + 1}" for i in range(x_new.shape[1])}
             labels['color'] = self.dataset.label_names[0]
             fig = px.scatter_matrix(x_new,
                                     color=y,
-                                    dimensions=range(self.n_components),
+                                    dimensions=range(x_new.shape[1]),
                                     labels=labels,
                                     **kwargs)
             fig.update_traces(diagonal_visible=False)

749aad1ac0e647d842aed9d2d32c09af5c7ddb74
João Correia <jfscorreia95@gmail.com>
2023-03-15 17:13:12 +0000

    [ADD] test for similarity matrix featurizer

---
+++ tests/unit_tests/featurizers/test_similarity_matrix.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from deepmol.compound_featurization.similarity_matrix import TanimotoSimilarityMatrix
+from unit_tests.featurizers.test_featurizers import FeaturizerTestCase
+
+
+class TestSimilarityMatrix(FeaturizerTestCase, TestCase):
+
+    def test_featurize(self):
+        dataset_rows_number = len(self.mock_dataset.mols)
+        TanimotoSimilarityMatrix(n_molecules=dataset_rows_number).featurize(self.mock_dataset)
+        self.assertEqual(dataset_rows_number, self.mock_dataset._X.shape[0])
+        self.assertEqual(dataset_rows_number, self.mock_dataset._X.shape[1])

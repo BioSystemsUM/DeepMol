4b3d100b68b7a920093e1e9d9ed8af3920c4a449
João Correia <jfscorreia95@gmail.com>
2023-03-15 17:00:49 +0000

    [ADD] methods to calculate morgan fingerprints and to compute tanimoto similarities

--- src/deepmol/compound_featurization/_utils.py
+++ src/deepmol/compound_featurization/_utils.py
@@ -1,7 +1,9 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
+import numpy as np
 from deepchem.utils import ConformerGenerator
-from rdkit.Chem import Mol
+from rdkit import DataStructs
+from rdkit.Chem import Mol, AllChem
 
 from deepmol.loggers import Logger
 
@@ -85,3 +87,52 @@ def get_dictionary_from_smiles(smiles: List[str], max_len: int) -> Dict[str, int
     unique_char_list = list(char_set) + [pad_token, out_of_vocab_token]
     dictionary = {letter: idx for idx, letter in enumerate(unique_char_list)}
     return dictionary
+
+
+def calc_morgan_fingerprints(mols: np.ndarray, **kwargs) -> List[np.ndarray]:
+    """
+    Calculates Morgan fingerprints for a array of molecules.
+
+    Parameters
+    ----------
+    mols: np.ndarray
+        Array of rdkit mol objects
+    kwargs:
+        Keyword arguments for the Morgan fingerprint calculation.
+
+    Returns
+    -------
+    fps: List[np.ndarray]
+        List of Morgan fingerprints
+    """
+    if "radius" not in kwargs:
+        kwargs["radius"] = 2
+    if "nBits" not in kwargs:
+        kwargs["nBits"] = 1024
+    fps = [AllChem.GetMorganFingerprintAsBitVect(m, **kwargs) for m in mols]
+    return fps
+
+
+def calc_similarity(first_fp_idx: int, second_fp_idx: int,  fps: List[np.ndarray]) -> Tuple[int, int, float]:
+    """
+    Calculates the Tanimoto similarity between two fingerprints.
+
+    Parameters
+    ----------
+    first_fp_idx: int
+        Index of the first fingerprint
+    second_fp_idx: int
+        Index of the second fingerprint
+    fps: List[np.ndarray]
+        List of Morgan fingerprints
+
+    Returns
+    -------
+    i: int
+        Index of the first fingerprint
+    j: int
+        Index of the second fingerprint
+    similarity: float
+        Tanimoto similarity between the two fingerprints
+    """
+    return first_fp_idx, second_fp_idx, DataStructs.TanimotoSimilarity(fps[first_fp_idx], fps[second_fp_idx])

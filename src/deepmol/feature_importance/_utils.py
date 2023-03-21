c515aa2c3cbefa96810fb39ae7e2380e3f913186
João Correia <jfscorreia95@gmail.com>
2023-03-21 15:26:43 +0000

    [ADD] shap utils

--- src/deepmol/feature_importance/_utils.py
+++ src/deepmol/feature_importance/_utils.py
@@ -1,32 +1,23 @@
-from typing import Union
-
 from shap import Explainer
-from shap.explainers import Exact
-from shap.explainers.other import Random
-from shap.maskers import Independent, Masker
-from shap.maskers import Partition
+from shap.explainers import Additive, Exact, GPUTree, Permutation, Partition, Tree, Linear, Sampling, Deep
+from shap.maskers import Independent, Impute, Text, Masker
+from shap.maskers import Partition as PartitionM
 
-from shap import TreeExplainer, DeepExplainer, LinearExplainer, KernelExplainer, GPUTreeExplainer, \
-    PermutationExplainer, PartitionExplainer, SamplingExplainer, AdditiveExplainer
-
-from deepmol.models import Model
-
-explainers = {'explainer': Explainer,
-              'kernel': KernelExplainer,
-              'sampling': SamplingExplainer,
-              'tree': TreeExplainer,
-              'gpu_tree': GPUTreeExplainer,
-              'deep': DeepExplainer,
-              'linear': LinearExplainer,
-              'partition': PartitionExplainer,
-              'permutation': PermutationExplainer,
-              'additive': AdditiveExplainer,
+explainers = {'additive': Additive,
               'exact': Exact,
-              'random': Random,
+              'gpu_tree': GPUTree,  # not sure if it is an explainer
+              'permutation': Permutation,
+              'partition': Partition,
+              'tree': Tree,
+              'linear': Linear,
+              'sampling': Sampling,
+              'deep': Deep
               }
 
-maskers = {'independent': Independent,
-           'partition': Partition,
+maskers = {'independent': Independent,  # data, max_samples=100
+           'partition': PartitionM,  # data, max_samples=100, clustering="correlation"
+           'impute': Impute,  # data, method="linear"
+           'text': Text,  # tokenizer=None, mask_token=None, collapse_mask_token="auto", output_type="string"
            }
 
 
@@ -44,9 +35,6 @@ def str_to_explainer(explainer: str) -> Explainer:
     explainer: Explainer
         The SHAP explainer.
     """
-    if explainer not in explainers.keys():
-        raise ValueError(f'Explainer {explainer} not supported. '
-                         f'Available explainers: {list(explainers.keys())}')
     return explainers[explainer]
 
 
@@ -64,35 +52,36 @@ def str_to_masker(masker: str) -> Masker:
     masker: Masker
         The SHAP masker.
     """
-    if masker not in maskers.keys():
-        raise ValueError(f'Masker {masker} not supported. '
-                         f'Available maskers: {list(maskers.keys())}')
     return maskers[masker]
 
 
-def get_model_instance_from_explainer(explainer: str, model: Model) -> Union[Model, callable]:
+def masker_args(masker: str, **kwargs) -> dict:
     """
-    Get the model instance/prediction function based on the explainer type.
+    Get the arguments of a SHAP masker.
 
     Parameters
     ----------
-    explainer: str
-        The name of the explainer
-    model: Model
-        The model to explain
+    masker: str
+        The name of the masker
+    **kwargs: dict
+        Additional arguments for the masker.
 
     Returns
     -------
-    model_instance: Model or callable
-        The model instance or prediction function.
+    args: dict
+        The arguments of the masker.
     """
-    if explainer in ['tree', 'linear']:
-        return model.model
-    if explainer == 'gpu_tree':
-        raise ValueError('Tree explainer not supported yet! Referring to '
-                         'https://github.com/slundberg/shap/issues/1136 and '
-                         'https://github.com/slundberg/shap/issues/1650')
-    elif explainer == 'deep':
-        return model.model.model
+    if masker == 'independent':
+        return {'max_samples': kwargs.get('max_samples', 100)}
+    elif masker == 'partition':
+        return {'max_samples': kwargs.get('max_samples', 100),
+                'clustering': kwargs.get('clustering', 'correlation')}
+    elif masker == 'impute':
+        return {'method': kwargs.get('method', 'linear')}
+    elif masker == 'text':
+        return {'tokenizer': kwargs.get('tokenizer', None),
+                'mask_token': kwargs.get('mask_token', None),
+                'collapse_mask_token': kwargs.get('collapse_mask_token', 'auto'),
+                'output_type': kwargs.get('output_type', 'string')}
     else:
-        return model.model.predict
+        raise ValueError(f'Unknown masker: {masker}')

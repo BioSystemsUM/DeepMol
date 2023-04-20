82b7357aa273cd017a01e3aff2bc4eefbe05bbf2
João Capela <pg38274@alunos.uminho.pt>
2023-04-20 12:11:14 +0100

    [REFACTOR] drawing bits in molecules - still under preparation

--- tests/unit_tests/featurizers/test_rdkit_fingerprints.py
+++ tests/unit_tests/featurizers/test_rdkit_fingerprints.py
@@ -1,10 +1,16 @@
+import os
 from copy import copy
 from unittest import TestCase
+
+from IPython.core.display import SVG
+from rdkit.Chem import MolFromSmiles
 
 from deepmol.compound_featurization import MorganFingerprint, \
     MACCSkeysFingerprint, \
     LayeredFingerprint, RDKFingerprint, AtomPairFingerprint
 from tests.unit_tests.featurizers.test_featurizers import FeaturizerTestCase
+
+from tests import TEST_DIR
 
 
 class TestRDKitFingerprints(FeaturizerTestCase, TestCase):
@@ -49,3 +55,58 @@ class TestRDKitFingerprints(FeaturizerTestCase, TestCase):
         dataset = copy(self.mock_dataset_with_invalid)
         RDKFingerprint().featurize(dataset)
         self.assertEqual(dataset_rows_number, dataset._X.shape[0])
+
+    def test_morgan_fingerprint_draw_bit(self):
+        dataset = copy(self.mock_dataset)
+        molecule = dataset.mols[0]
+        morgan_fingerprint = MorganFingerprint()
+        depiction = morgan_fingerprint.draw_bit(molecule, 1,
+                                                file_path=os.path.join(TEST_DIR, "data",
+                                                                       'test_morgan_fingerprint_draw_bit.svg'))
+
+        self.assertTrue(isinstance(depiction, SVG))
+        self.assertTrue(os.path.exists(os.path.join(TEST_DIR, "data", 'test_morgan_fingerprint_draw_bit.svg')))
+        os.remove(os.path.join(TEST_DIR, "data", 'test_morgan_fingerprint_draw_bit.svg'))
+
+    def test_morgan_fingerprints_draw_bits(self):
+        dataset = copy(self.mock_dataset)
+        molecule = dataset.mols[0]
+        morgan_fingerprint = MorganFingerprint()
+        depiction = morgan_fingerprint.draw_bits(molecule, [1, 114, 227], file_path=os.path.join(TEST_DIR,
+                                                                                                 "data",
+                                                                                                 'test_morgan_fingerprint_draw_bit.svg'))
+        self.assertTrue(isinstance(depiction, SVG))
+        self.assertTrue(os.path.exists(os.path.join(TEST_DIR, "data", 'test_morgan_fingerprint_draw_bit.svg')))
+
+        depiction = morgan_fingerprint.draw_bits(molecule, "ON", file_path=os.path.join(TEST_DIR,
+                                                                                        "data",
+                                                                                        'test_morgan_fingerprint_draw_bit.svg'))
+        self.assertTrue(isinstance(depiction, SVG))
+        self.assertTrue(os.path.exists(os.path.join(TEST_DIR, "data", 'test_morgan_fingerprint_draw_bit.svg')))
+
+        depiction = morgan_fingerprint.draw_bits(molecule, 1, file_path=os.path.join(TEST_DIR,
+                                                                                        "data",
+                                                                                        'test_morgan_fingerprint_draw_bit.svg'))
+        self.assertTrue(isinstance(depiction, SVG))
+        self.assertTrue(os.path.exists(os.path.join(TEST_DIR, "data", 'test_morgan_fingerprint_draw_bit.svg')))
+        os.remove(os.path.join(TEST_DIR, "data", 'test_morgan_fingerprint_draw_bit.svg'))
+
+    def test_morgan_fingerprint_draw_bit_with_invalid_molecule(self):
+        dataset = copy(self.mock_dataset_with_invalid)
+        molecule = dataset.smiles[-1]
+        molecule = MolFromSmiles(molecule)
+        morgan_fingerprint = MorganFingerprint()
+        self.assertRaises(ValueError, morgan_fingerprint.draw_bit, molecule, 1,
+                          file_path=os.path.join(TEST_DIR, "data", 'test_morgan_fingerprint_draw_bit.svg'))
+
+    def test_macckeys_fingerprint_draw_bit(self):
+        dataset = copy(self.mock_dataset)
+        molecule = dataset.mols[0]
+        morgan_fingerprint = MACCSkeysFingerprint()
+        depiction = morgan_fingerprint.draw_bit(molecule, 1,
+                                                file_path=os.path.join(TEST_DIR, "data",
+                                                                       'test_macckeys_fingerprint_draw_bit.svg'))
+
+        self.assertTrue(isinstance(depiction, SVG))
+        self.assertTrue(os.path.exists(os.path.join(TEST_DIR, "data", 'test_macckeys_fingerprint_draw_bit.svg')))
+        os.remove(os.path.join(TEST_DIR, "data", 'test_macckeys_fingerprint_draw_bit.svg'))

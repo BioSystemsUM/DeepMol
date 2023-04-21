46a124d6f70245e0522c0cc817fef691e2c9494a
João Correia <jfscorreia95@gmail.com>
2023-04-21 16:36:58 +0100

    [ADD] cross_validate test

--- tests/unit_tests/models/test_deepchem_model.py
+++ tests/unit_tests/models/test_deepchem_model.py
@@ -1,5 +1,6 @@
 import os
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 import numpy as np
 from deepchem.feat import ConvMolFeaturizer
@@ -7,13 +8,13 @@ from deepchem.models import GraphConvModel
 from rdkit.Chem import MolFromSmiles
 from sklearn.metrics import f1_score
 
+from deepmol.datasets import SmilesDataset
 from deepmol.metrics.metrics_functions import roc_auc_score, precision_score, accuracy_score, confusion_matrix
 
 from deepmol.metrics import Metric
 from deepmol.models import DeepChemModel
+from deepmol.splitters import RandomSplitter
 from unit_tests.models.test_models import ModelsTestCase
-
-import tensorflow as tf
 
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
@@ -84,3 +85,41 @@ class TestDeepChemModel(ModelsTestCase, TestCase):
         f1_score_value = f1_score(self.multitask_dataset_test.y, np.round(np.array(y_preds)), average='micro')
         self.assertEqual(precision_score_value, evaluate[0]["precision_score"])
         self.assertEqual(f1_score_value, evaluate[0]["f1_score"])
+
+    def test_cross_validate(self):
+        ds_train = self.binary_dataset
+        ds_train.X = ConvMolFeaturizer().featurize([MolFromSmiles('CCC')] * 100)
+        ds_train.select_to_split.side_effect = lambda arg: MagicMock(spec=SmilesDataset,
+                                                                     X=ds_train.X[arg],
+                                                                     y=ds_train.y[arg],
+                                                                     n_tasks=1,
+                                                                     label_names=['binary_label'],
+                                                                     mode='classification',
+                                                                     ids=ds_train.ids[arg])
+
+        graph = GraphConvModel(n_tasks=1, mode='classification')
+        model = DeepChemModel(graph)
+
+        best_model, train_score_best_model, test_score_best_model, train_scores, test_scores, avg_train_score, \
+            avg_test_score = model.cross_validate(ds_train, metric=Metric(roc_auc_score), folds=3)
+        self.assertIsNotNone(best_model)
+        self.assertIsInstance(train_score_best_model, float)
+        self.assertIsInstance(test_score_best_model, float)
+        self.assertEqual(len(train_scores), 3)
+        self.assertEqual(len(test_scores), 3)
+        self.assertIsInstance(avg_train_score, float)
+        self.assertIsInstance(avg_test_score, float)
+
+        splitter = RandomSplitter()
+        best_model, train_score_best_model, test_score_best_model, train_scores, test_scores, avg_train_score, \
+            avg_test_score = model.cross_validate(self.binary_dataset,
+                                                  metric=Metric(roc_auc_score),
+                                                  splitter=splitter,
+                                                  folds=3)
+        self.assertIsNotNone(best_model)
+        self.assertIsInstance(train_score_best_model, float)
+        self.assertIsInstance(test_score_best_model, float)
+        self.assertEqual(len(train_scores), 3)
+        self.assertEqual(len(test_scores), 3)
+        self.assertIsInstance(avg_train_score, float)
+        self.assertIsInstance(avg_test_score, float)

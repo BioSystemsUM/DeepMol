f2294789ef57fdead236f4748820f6d269f8a438
João Correia <jfscorreia95@gmail.com>
2023-04-21 18:08:55 +0100

    [ADD] RandomOverSampler and SMOTE test

--- tests/unit_tests/imbalanced_learn/test_imbalanced_learn.py
+++ tests/unit_tests/imbalanced_learn/test_imbalanced_learn.py
@@ -3,11 +3,11 @@ import os
 from unittest import TestCase
 
 import numpy as np
-from imblearn.over_sampling import SMOTE as SMOTE_IB
+import pandas as pd
 
 from deepmol.datasets import SmilesDataset
-from deepmol.imbalanced_learn import RandomOverSampler, SMOTE, ClusterCentroids, RandomUnderSampler, SMOTEENN, \
-    SMOTETomek
+from deepmol.imbalanced_learn import RandomOverSampler, SMOTE
+from tests import TEST_DIR
 from unit_tests._mock_utils import SmilesDatasetMagicMock
 
 
@@ -15,11 +15,12 @@ class TestImbalancedLearn(TestCase):
 
     def setUp(self) -> None:
         # create a dataset with 10 samples and 5 features
-        x = np.random.randint(0, 10, size=(14, 5))
+        x = np.random.randint(0, 10, size=(10, 5))
         # y with 8 samples of class 0 and 2 samples of class 1
-        y = np.array([1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0])
+        y = np.zeros(8)
+        y = np.append(y, np.ones(2))
         # ids 10 characters from the alphabet
-        ids = np.array([''.join(np.random.choice(list('abcdefghij'), 14)) for _ in range(14)])
+        ids = np.array([''.join(np.random.choice(list('abcdefghij'), 10)) for _ in range(10)])
         self.imbalanced_dataset = SmilesDatasetMagicMock(spec=SmilesDataset,
                                                          X=x,
                                                          y=y,
@@ -40,49 +41,9 @@ class TestImbalancedLearn(TestCase):
 
     def test_smote(self):
         df = copy.deepcopy(self.imbalanced_dataset)
-        new_df = SMOTE(k_neighbors=1).sample(df)
+        new_df = SMOTE().sample(df)
         self.assertTrue(new_df._X.shape[0] > df.X.shape[0])
         # assert under-represented class is oversampled
         self.assertTrue(np.sum(new_df._y == 1) > np.sum(df.y == 1))
-        # assert ids are not duplicated
-        self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
-
-    def test_cluster_centroids(self):
-        df = copy.deepcopy(self.imbalanced_dataset)
-        new_df = ClusterCentroids().sample(df)
-        self.assertTrue(new_df._X.shape[0] < df.X.shape[0])
-        # assert over-represented class is undersampled
-        self.assertTrue(np.sum(new_df._y == 0) < np.sum(df.y == 0))
-        # assert ids are not duplicated
-        self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
-
-    def test_random_under_sampler(self):
-        df = copy.deepcopy(self.imbalanced_dataset)
-        new_df = RandomUnderSampler().sample(df)
-        self.assertTrue(new_df._X.shape[0] < df.X.shape[0])
-        # assert over-represented class is undersampled
-        self.assertTrue(np.sum(new_df._y == 0) < np.sum(df.y == 0))
-        # assert ids are not duplicated
-        self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
-
-    def test_SMOTEENN(self):
-        df = copy.deepcopy(self.imbalanced_dataset)
-        smote = SMOTE_IB(k_neighbors=1)
-        new_df = SMOTEENN(smote=smote).sample(df)
-        # assert under-represented class is oversampled
-        self.assertTrue(np.sum(new_df._y == 1) > np.sum(df.y == 1))
-        # assert over-represented class is undersampled
-        self.assertTrue(np.sum(new_df._y == 0) < np.sum(df.y == 0))
-        # assert ids are not duplicated
-        self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
-
-    def test_SMOTETomek(self):
-        df = copy.deepcopy(self.imbalanced_dataset)
-        smote = SMOTE_IB(k_neighbors=2)
-        new_df = SMOTETomek(smote=smote).sample(df)
-        # assert under-represented class is oversampled
-        self.assertTrue(np.sum(new_df._y == 1) > np.sum(df.y == 1))
-        # assert over-represented class is undersampled
-        self.assertTrue(np.sum(new_df._y == 0) <= np.sum(df.y == 0))
         # assert ids are not duplicated
         self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))

06abc8e719a5e207dcaa67bc5ef23514ada80e59
João Correia <jfscorreia95@gmail.com>
2023-04-26 14:03:50 +0100

    [ADD] copy_on_write_decorator

--- src/deepmol/compound_featurization/similarity_matrix.py
+++ src/deepmol/compound_featurization/similarity_matrix.py
@@ -5,6 +5,7 @@ import numpy as np
 from deepmol.compound_featurization._utils import calc_morgan_fingerprints, calc_similarity
 from deepmol.datasets import Dataset
 from deepmol.parallelism.multiprocessing import JoblibMultiprocessing
+from deepmol.utils.decorators import copy_on_write_decorator
 
 
 class TanimotoSimilarityMatrix:
@@ -46,6 +47,7 @@ class TanimotoSimilarityMatrix:
         """
         return calc_similarity(i, j, self.fps)
 
+    @copy_on_write_decorator
     def featurize(self,
                   dataset: Dataset,
                   **kwargs

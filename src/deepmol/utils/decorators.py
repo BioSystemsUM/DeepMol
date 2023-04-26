41c473a21f894ee9c77bd0a5ad2504f67f7ced21
João Correia <jfscorreia95@gmail.com>
2023-04-26 11:21:50 +0100

    [ADD] copy_on_write_decorator to do inplace, or not, operations on objects of other classes

--- src/deepmol/utils/decorators.py
+++ src/deepmol/utils/decorators.py
@@ -1,4 +1,7 @@
-def inplace_decorator(method: callable) -> callable:
+from typing import Union
+
+
+def inplace_decorator(method: callable) -> Union[callable, None]:
     """
     Decorator to make a method inplace.
 
@@ -13,6 +16,25 @@ def inplace_decorator(method: callable) -> callable:
         Decorated method.
     """
     def inplace_method(self, *args, inplace=False, **kwargs):
+        """
+        Method to make inplace.
+
+        Parameters
+        ----------
+        self: object
+            Object to apply the method to.
+        args: list
+            Arguments to pass to the method.
+        inplace: bool
+            Whether to apply the method inplace.
+        kwargs: dict
+            Keyword arguments to pass to the method.
+
+        Returns
+        -------
+        result: object
+            Result of the method.
+        """
         if inplace:
             method(self, *args, **kwargs)
             return None
@@ -21,3 +43,47 @@ def inplace_decorator(method: callable) -> callable:
             method(result, *args, **kwargs)
             return result
     return inplace_method
+
+
+def copy_on_write_decorator(original_func: callable) -> Union[callable, None]:
+    """
+    Decorator to make a method copy-on-write.
+
+    Parameters
+    ----------
+    original_func: callable
+        Method to decorate.
+
+    Returns
+    -------
+    new_func: callable
+        Decorated method.
+    """
+    def new_func(self, other_object, inplace=False):
+        """
+        Method to make copy-on-write.
+
+        Parameters
+        ----------
+        self: object
+            the class instance object
+        other_object: object
+            Object to apply the method to.
+        inplace: bool
+            Whether to apply the method inplace.
+
+        Returns
+        -------
+        new_object: object
+            New object.
+        """
+        if inplace:
+            # modify the other_object in-place
+            original_func(self, other_object)
+            return None
+        else:
+            # create a new copy of the other_object
+            new_object = other_object.__copy__()
+            original_func(self, new_object)
+            return new_object
+    return new_func

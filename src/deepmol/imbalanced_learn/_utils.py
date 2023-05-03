dc46cdd19384b3312da67ddda9748c947a0d45ce
João Correia <jfscorreia95@gmail.com>
2023-05-03 18:15:58 +0100

    [FIX] tracking of smiles and mols was not being done

--- src/deepmol/imbalanced_learn/_utils.py
+++ src/deepmol/imbalanced_learn/_utils.py
@@ -3,7 +3,11 @@ import uuid
 import numpy as np
 
 
-def _get_new_ids(original_x: np.array, new_x, original_ids):
+def _get_new_ids_smiles_mols(original_x: np.array,
+                             new_x: np.array,
+                             original_ids: np.array,
+                             original_smiles: np.array,
+                             original_mols: np.array) -> (np.array, np.array, np.array):
     """
     Get new ids for the new dataset.
 
@@ -15,17 +19,43 @@ def _get_new_ids(original_x: np.array, new_x, original_ids):
         Sampled dataset.
     original_ids: np.array
         Original ids.
+    original_smiles: np.array
+        Original smiles.
+    original_mols: np.array
+        Original mols.
 
     Returns
     -------
-    np.array
+    ids: np.array
         Ids of the sampled dataset.
+    smiles: np.array
+        Smiles of the sampled dataset.
+    mols: np.array
+        Mols of the sampled dataset.
     """
     ids = []
+    smiles = []
+    mols = []
+    assigned_ids = set()
     for i in range(new_x.shape[0]):
-        # check if the sample is artificial or not be checcking if the row is in the original dataset
+        # check if the sample is artificial or not by checking if the row is in the original dataset
         if np.any(np.all(new_x[i] == original_x, axis=1)):
-            ids.append(original_ids[np.where(np.all(new_x[i] == original_x, axis=1))[0][0]])
+            idx = np.where(np.all(new_x[i] == original_x, axis=1))[0][0]
+            id_ = original_ids[idx]
+            # check if id has already been assigned to another sample
+            if id_ in assigned_ids:
+                id_ = f"ib_{id_}"
+            assigned_ids.add(id_)
+            ids.append(id_)
+            smiles.append(original_smiles[idx])
+            mols.append(original_mols[idx])
         else:
-            ids.append(f"ib_{uuid.uuid4().hex}")
-    return np.array(ids)
+            id_ = f"ib_{uuid.uuid4().hex}"
+            # check if id has already been assigned to another sample
+            while id_ in assigned_ids:
+                id_ = f"ib_{uuid.uuid4().hex}"
+            assigned_ids.add(id_)
+            ids.append(id_)
+            smiles.append(None)
+            mols.append(None)
+    return np.array(ids), np.array(smiles), np.array(mols)
--- tests/unit_tests/imbalanced_learn/test_imbalanced_learn.py
+++ tests/unit_tests/imbalanced_learn/test_imbalanced_learn.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 
 import numpy as np
 from imblearn.over_sampling import SMOTE as SMOTE_IB
+from rdkit.Chem import MolFromSmiles
 
 from deepmol.datasets import SmilesDataset
 from deepmol.imbalanced_learn import RandomOverSampler, SMOTE, ClusterCentroids, RandomUnderSampler, SMOTEENN, \
@@ -14,14 +15,20 @@ from unit_tests._mock_utils import SmilesDatasetMagicMock
 class TestImbalancedLearn(TestCase):
 
     def setUp(self) -> None:
-        # create a dataset with 10 samples and 5 features
+        # create a dataset with 14 samples and 5 features
         x = np.random.randint(0, 10, size=(14, 5))
-        # y with 8 samples of class 0 and 2 samples of class 1
+        # y with 10 samples of class 0 and 4 samples of class 1
         y = np.array([1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0])
         # ids 10 characters from the alphabet
         ids = np.array([''.join(np.random.choice(list('abcdefghij'), 14)) for _ in range(14)])
+        # smiles with one more C than the last one
+        smiles = np.array(['C' * (i + 1) for i in range(14)])
+        # mols
+        mols = np.array([MolFromSmiles(s) for s in smiles])
         self.imbalanced_dataset = SmilesDatasetMagicMock(spec=SmilesDataset,
                                                          X=x,
+                                                         smiles=smiles,
+                                                         mols=mols,
                                                          y=y,
                                                          ids=ids)
 
@@ -37,6 +44,7 @@ class TestImbalancedLearn(TestCase):
         self.assertTrue(np.sum(new_df._y == 1) > np.sum(df.y == 1))
         # assert ids are not duplicated
         self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
+        self.assertEqual(len(new_df._ids), len(new_df._smiles), len(new_df._mols))
 
     def test_smote(self):
         df = copy.deepcopy(self.imbalanced_dataset)
@@ -46,6 +54,7 @@ class TestImbalancedLearn(TestCase):
         self.assertTrue(np.sum(new_df._y == 1) > np.sum(df.y == 1))
         # assert ids are not duplicated
         self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
+        self.assertEqual(len(new_df._ids), len(new_df._smiles), len(new_df._mols))
 
     def test_cluster_centroids(self):
         df = copy.deepcopy(self.imbalanced_dataset)
@@ -55,6 +64,7 @@ class TestImbalancedLearn(TestCase):
         self.assertTrue(np.sum(new_df._y == 0) < np.sum(df.y == 0))
         # assert ids are not duplicated
         self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
+        self.assertEqual(len(new_df._ids), len(new_df._smiles), len(new_df._mols))
 
     def test_random_under_sampler(self):
         df = copy.deepcopy(self.imbalanced_dataset)
@@ -64,25 +74,28 @@ class TestImbalancedLearn(TestCase):
         self.assertTrue(np.sum(new_df._y == 0) < np.sum(df.y == 0))
         # assert ids are not duplicated
         self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
+        self.assertEqual(len(new_df._ids), len(new_df._smiles), len(new_df._mols))
 
     def test_SMOTEENN(self):
         df = copy.deepcopy(self.imbalanced_dataset)
         smote = SMOTE_IB(k_neighbors=1)
         new_df = SMOTEENN(smote=smote).sample(df)
         # assert under-represented class is oversampled
-        self.assertTrue(np.sum(new_df._y == 1) > np.sum(df.y == 1))
+        self.assertTrue(np.sum(new_df._y == 1) >= np.sum(df.y == 1))
         # assert over-represented class is undersampled
-        self.assertTrue(np.sum(new_df._y == 0) < np.sum(df.y == 0))
+        self.assertTrue(np.sum(new_df._y == 0) <= np.sum(df.y == 0))
         # assert ids are not duplicated
         self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
+        self.assertEqual(len(new_df._ids), len(new_df._smiles), len(new_df._mols))
 
     def test_SMOTETomek(self):
         df = copy.deepcopy(self.imbalanced_dataset)
         smote = SMOTE_IB(k_neighbors=2)
         new_df = SMOTETomek(smote=smote).sample(df)
         # assert under-represented class is oversampled
-        self.assertTrue(np.sum(new_df._y == 1) > np.sum(df.y == 1))
+        self.assertTrue(np.sum(new_df._y == 1) >= np.sum(df.y == 1))
         # assert over-represented class is undersampled
         self.assertTrue(np.sum(new_df._y == 0) <= np.sum(df.y == 0))
         # assert ids are not duplicated
         self.assertEqual(len(np.unique(new_df._ids)), len(new_df._ids))
+        self.assertEqual(len(new_df._ids), len(new_df._smiles), len(new_df._mols))

83b323ae7e8a8e6c7ff8b9450f43bd1dab2fb2d2
João Correia <jfscorreia95@gmail.com>
2023-05-30 18:19:20 +0100

    [REFACTOR] base tokenizer

--- src/deepmol/tokenizers/tokenizer.py
+++ src/deepmol/tokenizers/tokenizer.py
@@ -1,17 +1,18 @@
-import multiprocessing
 from abc import ABC, abstractmethod
 
-import numpy as np
+from deepmol.base import Estimator
+from deepmol.datasets import Dataset
+from deepmol.parallelism.multiprocessing import JoblibMultiprocessing
 
 
-class Tokenizer(ABC):
+class Tokenizer(Estimator, ABC):
     """
     An abstract class for tokenizers.
-    Tokenizers are used to encode and decode strings.
-    Child classes must implement the encode and decode methods and the shape property.
+    Tokenizers are used to tokenize strings.
+    Child classes must implement the tokenize method.
     """
 
-    def __init__(self, n_jobs=-1):
+    def __init__(self, n_jobs: int) -> None:
         """
         Initializes the tokenizer.
 
@@ -20,81 +21,67 @@ class Tokenizer(ABC):
         n_jobs: int
             The number of jobs to run in parallel in the featurization.
         """
-        self.n_jobs = n_jobs if n_jobs > 0 else multiprocessing.cpu_count()
+        super().__init__()
+        self.n_jobs = n_jobs
+
+    def tokenize(self, dataset: Dataset) -> list:
+        """
+        Tokenizes a dataset.
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to tokenize.
+
+        Returns
+        -------
+        dataset: Dataset
+            The tokenized dataset.
+        """
+        if not self._is_fitted:
+            raise ValueError("The tokenizer must be fitted before tokenizing a dataset. "
+                             "Call Tokenizer.fit(dataset) first.")
+        smiles = dataset.smiles
+        multiprocessing_cls = JoblibMultiprocessing(process=self._tokenize, n_jobs=self.n_jobs)
+        tokens = multiprocessing_cls.run(smiles)
+        return list(tokens)
 
     @abstractmethod
-    def encode(self, smiles: str) -> np.ndarray:
+    def _tokenize(self, text: str) -> list:
         """
-        Encodes a SMILES string into a one-hot encoded matrix.
+        Tokenizes a text.
 
         Parameters
         ----------
-        smiles: str
-            The SMILES string to encode.
+        text: str
+            The text to tokenize.
 
         Returns
         -------
-        smiles_matrix: np.ndarray
-            The one-hot encoded matrix.
+        tokens: list
+            The list of tokens.
         """
-
-    def _encode_smiles_parallel(self, smiles_list: list) -> list:
-        """
-        Encodes a list of SMILES strings into a list of one-hot encoded matrices.
-
-        Parameters
-        ----------
-        smiles_list: list
-            The list of SMILES strings to encode.
-
-        Returns
-        -------
-        smiles_matrix_list: list
-            The list of one-hot encoded matrices.
-        """
-        with multiprocessing.Pool(processes=self.n_jobs) as pool:
-            results = [pool.apply_async(self.encode, args=(sm,)) for sm in smiles_list]
-            encoded_smiles_list = [result.get() for result in results]
-        return encoded_smiles_list
-
-    @abstractmethod
-    def decode(self, smiles_matrix: np.ndarray) -> str:
-        """
-        Decodes a one-hot encoded matrix into a SMILES string.
-
-        Parameters
-        ----------
-        smiles_matrix: np.ndarray
-            The one-hot encoded matrix to decode.
-
-        Returns
-        -------
-        smiles: str
-            The decoded SMILES string.
-        """
-
-    def _decode_smiles_parallel(self, smiles_matrix_list: list) -> list:
-        """
-        Decodes a list of one-hot encoded matrices into a list of SMILES strings using multiprocessing.
-
-        Parameters
-        ----------
-        smiles_matrix_list: list
-            The list of one-hot encoded matrices to decode.
-
-        Returns
-        -------
-        smiles_list: list
-            The list of decoded SMILES strings.
-        """
-        with multiprocessing.Pool(processes=self.n_jobs) as pool:
-            results = [pool.apply_async(self.decode, args=(smiles_matrix,)) for smiles_matrix in smiles_matrix_list]
-            decoded_smiles_list = [result.get() for result in results]
-        return decoded_smiles_list
 
     @property
     @abstractmethod
-    def shape(self) -> tuple:
+    def vocabulary(self) -> list:
         """
-        Returns the shape of the one-hot encoded matrix.
+        Returns the vocabulary.
+
+        Returns
+        -------
+        vocabulary: list
+            The vocabulary.
+        """
+
+    @property
+    @abstractmethod
+    def max_length(self) -> int:
+        """
+        Returns the maximum length of a tokenized string.
+
+        Returns
+        -------
+        max_length: int
+            The maximum length of a tokenized string.
         """

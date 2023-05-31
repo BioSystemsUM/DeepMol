ba09252923544ec16b1296f864306db5d7a1af42
João Correia <jfscorreia95@gmail.com>
2023-05-31 17:03:27 +0100

    [ADD] properties and support for test set

--- src/deepmol/pipeline_optimization/pipeline_optimization.py
+++ src/deepmol/pipeline_optimization/pipeline_optimization.py
@@ -3,6 +3,7 @@ from typing import Union
 import optuna
 
 from deepmol.datasets import Dataset
+from deepmol.metrics import Metric
 from deepmol.pipeline_optimization._utils import _get_preset
 
 
@@ -12,16 +13,19 @@ class PipelineOptimization:
         self.direction = direction
         self.study = optuna.create_study(direction=direction)
 
-    def optimize(self, dataset: Dataset, objective: Union[callable, str], n_trials: int):
+    def optimize(self, train_dataset: Dataset, test_dataset: Dataset, objective: Union[callable, str], metric: Metric, n_trials: int):
         if isinstance(objective, str):
-            objective = _get_preset(dataset, objective)
-        self.study.optimize(objective, n_trials=n_trials)
+            objective = _get_preset(train_dataset, objective)
+        self.study.optimize(lambda trial: objective(trial, train_dataset, test_dataset, metric), n_trials=n_trials)
 
+    @property
     def best_params(self):
         return self.study.best_params
 
+    @property
     def best_trial(self):
         return self.study.best_trial
 
+    @property
     def best_value(self):
         return self.study.best_value

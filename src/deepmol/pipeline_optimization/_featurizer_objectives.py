57d1ab93e38bb34d844f799707b29486ec245a34
João Correia <jfscorreia95@gmail.com>
2023-06-02 18:28:43 +0100

    [REMOVE] 3d descriptors to save time while testing

--- src/deepmol/pipeline_optimization/_featurizer_objectives.py
+++ src/deepmol/pipeline_optimization/_featurizer_objectives.py
@@ -6,7 +6,10 @@ from deepmol.base import Transformer
 from deepmol.compound_featurization import All3DDescriptors, TwoDimensionDescriptors, MorganFingerprint, \
     AtomPairFingerprint, LayeredFingerprint, RDKFingerprint, MACCSkeysFingerprint, Mol2Vec
 
-_1D_FEATURIZERS = {'3d_descriptors': All3DDescriptors, '2d_descriptors': TwoDimensionDescriptors,
+# _1D_FEATURIZERS = {'3d_descriptors': All3DDescriptors, '2d_descriptors': TwoDimensionDescriptors,
+#                    'morgan': MorganFingerprint, 'atom_pair': AtomPairFingerprint, 'layered': LayeredFingerprint,
+#                    'rdk': RDKFingerprint, 'maccs': MACCSkeysFingerprint, 'mol2vec': Mol2Vec}
+_1D_FEATURIZERS = {'2d_descriptors': TwoDimensionDescriptors,
                    'morgan': MorganFingerprint, 'atom_pair': AtomPairFingerprint, 'layered': LayeredFingerprint,
                    'rdk': RDKFingerprint, 'maccs': MACCSkeysFingerprint, 'mol2vec': Mol2Vec}
 
@@ -21,15 +24,15 @@ def _get_featurizer(trial: Trial, feat_type: Literal['1D', '2D', '3D']) -> Trans
             n_bits = trial.suggest_int('n_bits', 1024, 2048, step=1024)
             return MorganFingerprint(radius=radius, size=n_bits)
         elif feat == 'atom_pair':
-            n_bits = trial.suggest_int('n_bits', 1024, 2048, step=1024)
-            min_length = trial.suggest_int('min_length', 1, 3)
-            max_length = trial.suggest_int('max_length', 20, 50, step=10)
-            return AtomPairFingerprint(size=n_bits, min_length=min_length, max_length=max_length)
+            nBits = trial.suggest_int('nBits', 1024, 2048, step=1024)
+            minLength = trial.suggest_int('minLength', 1, 3)
+            maxLength = trial.suggest_int('maxLength', 20, 50, step=10)
+            return AtomPairFingerprint(nBits=nBits, minLength=minLength, maxLength=maxLength)
         elif feat == 'layered':
-            n_bits = trial.suggest_int('n_bits', 1024, 2048, step=1024)
-            min_path = trial.suggest_int('min_length', 1, 3)
-            max_path = trial.suggest_int('max_length', 5, 10)
-            return LayeredFingerprint(size=n_bits, min_path=min_path, max_path=max_path)
+            fpSize = trial.suggest_int('fpSize', 1024, 2048, step=1024)
+            minPath = trial.suggest_int('minPath', 1, 3)
+            maxPath = trial.suggest_int('maxPath', 5, 10)
+            return LayeredFingerprint(fpSize=fpSize, minPath=minPath, maxPath=maxPath)
         elif feat == 'rdk':
             fpSize = trial.suggest_int('fpSize', 1024, 2048, step=1024)
             min_path = trial.suggest_int('min_path', 1, 3)

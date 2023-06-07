3404b27a37a87572555e3da8a4b2d4e8e2f76884
João Correia <jfscorreia95@gmail.com>
2023-06-07 18:23:54 +0100

    [ADD] best pipeline property

--- src/deepmol/pipeline_optimization/pipeline_optimization.py
+++ src/deepmol/pipeline_optimization/pipeline_optimization.py
@@ -1,3 +1,4 @@
+import os
 from typing import Union, List
 
 import optuna
@@ -8,7 +9,9 @@ from optuna.study import StudyDirection
 
 from deepmol.datasets import Dataset
 from deepmol.metrics import Metric
+from deepmol.pipeline import Pipeline
 from deepmol.pipeline_optimization._utils import _get_preset
+from deepmol.pipeline_optimization.objective_wrapper import Objective
 
 
 class PipelineOptimization:
@@ -24,12 +27,15 @@ class PipelineOptimization:
         self.direction = direction
         self.study = optuna.create_study(storage=storage, sampler=sampler, pruner=pruner, study_name=study_name,
                                          direction=direction, load_if_exists=load_if_exists, directions=directions)
+        self.study.set_user_attr("best_scores", {})
 
-    def optimize(self, train_dataset: Dataset, test_dataset: Dataset, objective: Union[callable, str], metric: Metric,
-                 n_trials: int):
-        if isinstance(objective, str):
-            objective = _get_preset(objective)
-        self.study.optimize(lambda trial: objective(trial, train_dataset, test_dataset, metric), n_trials=n_trials)
+    def optimize(self, train_dataset: Dataset, test_dataset: Dataset, objective_steps: Union[callable, str],
+                 metric: Metric, n_trials: int, save_top_n: int = 1, **kwargs):
+        if isinstance(objective_steps, str):
+            objective_steps = _get_preset(objective_steps)
+        objective = Objective(objective_steps, self.study, self.direction, train_dataset, test_dataset, metric,
+                              save_top_n, **kwargs)
+        self.study.optimize(objective, n_trials=n_trials)
 
     @property
     def best_params(self):
@@ -46,3 +52,9 @@ class PipelineOptimization:
     @property
     def trials(self):
         return self.study.trials
+
+    @property
+    def best_pipeline(self):
+        best_trial_id = self.study.best_trial.number
+        path = os.path.join(self.study.study_name, f'trial_{best_trial_id}')
+        return Pipeline.load(path)

e1d15d431d30cee2b0b29b7e4a933e5cdbb7ce57
João Correia <jfscorreia95@gmail.com>
2023-06-16 14:51:33 +0100

    [REMOVE] KernelCenterer scaler as it requires square matrix

--- src/deepmol/pipeline_optimization/_scaler_objectives.py
+++ src/deepmol/pipeline_optimization/_scaler_objectives.py
@@ -3,9 +3,10 @@ from deepmol.scalers import StandardScaler, MinMaxScaler, MaxAbsScaler, Binarize
     KernelCenterer, QuantileTransformer, PowerTransformer
 
 # TODO: PolynomialFeatures only woks with square matrices
+# TODO: KernelCenter only woks with square matrices
 _SCALERS = {'standard_scaler': StandardScaler, 'min_max_scaler': MinMaxScaler, 'max_abs_scaler': MaxAbsScaler,
             'robust_scaler': RobustScaler, 'normalizer': Normalizer, 'binarizer': Binarizer,
-            'kernel_centerer': KernelCenterer, 'quantile_transformer': QuantileTransformer,
+            'quantile_transformer': QuantileTransformer,
             'power_transformer': PowerTransformer, 'pass_through_transformer': PassThroughTransformer}
 
 

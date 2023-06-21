356fc36b44b029f78952306d42ac8e60acda70be
João Correia <jfscorreia95@gmail.com>
2023-06-21 15:55:40 +0100

    [ADD] docstrings

--- src/deepmol/pipeline_optimization/_standardizer_objectives.py
+++ src/deepmol/pipeline_optimization/_standardizer_objectives.py
@@ -10,6 +10,19 @@ _STANDARDIZERS = {"basic_standardizer": BasicStandardizer,
 
 
 def _get_standardizer(trial) -> Transformer:
+    """
+    Get a Standardizer object for the Optuna optimization.
+
+    Parameters
+    ----------
+    trial : optuna.Trial
+        An Optuna trial object.
+
+    Returns
+    -------
+    Transformer
+        The Standardizer object step.
+    """
     standardizer = trial.suggest_categorical("standardizer", list(_STANDARDIZERS.keys()))
     if standardizer == "custom_standardizer":
         choice = trial.suggest_categorical("standardization_type", ["simple_standardisation", "heavy_standardisation"])

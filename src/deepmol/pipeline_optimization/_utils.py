4f0db9c8bd614b0f1454ee98f653da7980418440
João Correia <jfscorreia95@gmail.com>
2023-07-04 17:31:29 +0100

    [ADD] support for multiclass on deepchem models

--- src/deepmol/pipeline_optimization/_utils.py
+++ src/deepmol/pipeline_optimization/_utils.py
@@ -39,7 +39,7 @@ def _get_preset(preset: Literal['deepchem', 'sklearn', 'keras', 'all']) -> calla
         return preset_all_models
 
 
-def _cnn_model_steps(trial, n_tasks):
+def _cnn_model_steps(trial, n_tasks, n_classes):
     featurizer = _get_featurizer(trial, '1D')
     if featurizer.__class__.__name__ == 'TwoDimensionDescriptors' or \
             featurizer.__class__.__name__ == 'All3DDescriptors':
@@ -48,7 +48,8 @@ def _cnn_model_steps(trial, n_tasks):
         scaler = PassThroughTransformer()
     dims = 1
     n_features = len(featurizer.feature_names)
-    cnn_kwargs = {'n_tasks': n_tasks, 'mode': 'regression', 'n_features': n_features, 'dims': dims}
+    cnn_kwargs = {'n_tasks': n_tasks, 'mode': 'regression', 'n_features': n_features, 'dims': dims,
+                  'n_classes': n_classes}
     model_step = cnn_model_steps(trial=trial, cnn_kwargs=cnn_kwargs)
     featurizer = ('featurizer', featurizer)
     scaler = ('scaler', scaler)
@@ -56,7 +57,7 @@ def _cnn_model_steps(trial, n_tasks):
     return [featurizer, scaler, model]
 
 
-def _multitask_classifier_model_steps(trial, n_tasks):
+def _multitask_classifier_model_steps(trial, n_tasks, n_classes):
     featurizer = _get_featurizer(trial, '1D')
     if featurizer.__class__.__name__ == 'TwoDimensionDescriptors' or \
             featurizer.__class__.__name__ == 'All3DDescriptors':
@@ -64,7 +65,7 @@ def _multitask_classifier_model_steps(trial, n_tasks):
     else:
         scaler = PassThroughTransformer()
     n_features = len(featurizer.feature_names)
-    multitask_classifier_kwargs = {'n_tasks': n_tasks, 'n_features': n_features}
+    multitask_classifier_kwargs = {'n_tasks': n_tasks, 'n_features': n_features, 'n_classes': n_classes}
     model_step = multitask_classifier_model_steps(trial=trial,
                                                   multitask_classifier_kwargs=multitask_classifier_kwargs)
     featurizer = ('featurizer', featurizer)
@@ -151,12 +152,15 @@ def preset_deepchem_models(trial, data: Dataset) -> list:
     #  the custom_objects) (SAME WITH robust_multitask_regressor_model)
     n_tasks = data.n_tasks
     mode = data.mode
+    n_classes = len(set(data.y)) if mode == 'classification' else 1
     final_steps = [('standardizer', _get_standardizer(trial))]
     models = ["gat_model", "gcn_model", "attentive_fp_model", "pagtn_model", "chem_ception_model", "dag_model",
               "graph_conv_model", "smiles_to_vec_model", "text_cnn_model", "weave_model", "dmpnn_model"]
     if mode == 'classification':
         models.extend(["multitask_classifier_model"])  # , "multitask_irv_classifier_model",
         # "progressive_multitask_classifier_model", "robust_multitask_classifier_model", "sc_score_model"])
+        if n_classes > 2:
+            models.remove("text_cnn_model")
     elif mode == 'regression':
         models.extend(["dtnn_model", "mat_model", "multitask_regressor_model"])  # ,
         # "progressive_multitask_regressor_model", "robust_multitask_regressor_model"])
@@ -164,38 +168,38 @@ def preset_deepchem_models(trial, data: Dataset) -> list:
         raise ValueError("data mode must be either 'classification' or 'regression'")
     model_steps = trial.suggest_categorical("model_steps", models)
     if model_steps == "gat_model":
-        gat_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        gat_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_gat = gat_model_steps(trial=trial, gat_kwargs=gat_kwargs)
         final_steps.extend(steps_gat)
     elif model_steps == "gcn_model":
-        gcn_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        gcn_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_gcn = gcn_model_steps(trial=trial, gcn_kwargs=gcn_kwargs)
         final_steps.extend(steps_gcn)
     elif model_steps == "attentive_fp_model":
-        attentive_fp_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        attentive_fp_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_attentive = attentive_fp_model_steps(trial=trial, attentive_fp_kwargs=attentive_fp_kwargs)
         final_steps.extend(steps_attentive)
     elif model_steps == "pagtn_model":
-        patgn_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        patgn_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_patgn = pagtn_model_steps(trial=trial, pagtn_kwargs=patgn_kwargs)
         final_steps.extend(steps_patgn)
     elif model_steps == "mpnn_model":
-        mpnn_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        mpnn_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_mpnn = mpnn_model_steps(trial=trial, mpnn_kwargs=mpnn_kwargs)
         final_steps.extend(steps_mpnn)
     elif model_steps == "megnet_model":
-        megnet_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        megnet_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_megnet = megnet_model_steps(trial=trial, megnet_kwargs=megnet_kwargs)
         final_steps.extend(steps_megnet)
     elif model_steps == "cnn_model":
-        featurizer_scaler_model = _cnn_model_steps(trial, n_tasks)
+        featurizer_scaler_model = _cnn_model_steps(trial, n_tasks, n_classes)
         final_steps.extend(featurizer_scaler_model)
     elif model_steps == "multitask_classifier_model":
-        featurizer_scaler_model = _multitask_classifier_model_steps(trial, n_tasks)
+        featurizer_scaler_model = _multitask_classifier_model_steps(trial, n_tasks, n_classes)
         final_steps.extend(featurizer_scaler_model)
     elif model_steps == "multitask_irv_classifier_model":
         featurizer = _get_featurizer(trial, '1D')
-        multitask_irv_classifier_kwargs = {'n_tasks': n_tasks}
+        multitask_irv_classifier_kwargs = {'n_tasks': n_tasks, 'n_classes': n_classes}
         model_step = multitask_irv_classifier_model_steps(trial=trial,
                                                           multitask_irv_classifier_kwargs=multitask_irv_classifier_kwargs)
         featurizer = ('featurizer', featurizer)
@@ -203,7 +207,7 @@ def preset_deepchem_models(trial, data: Dataset) -> list:
     elif model_steps == "progressive_multitask_classifier_model":
         featurizer = _get_featurizer(trial, '1D')
         n_features = len(featurizer.feature_names)
-        progressive_multitask_classifier_kwargs = {'n_tasks': n_tasks, 'n_features': n_features}
+        progressive_multitask_classifier_kwargs = {'n_tasks': n_tasks, 'n_features': n_features, 'n_classes': n_classes}
         model_step = progressive_multitask_classifier_model_steps(trial=trial,
                                                                   progressive_multitask_classifier_kwargs=progressive_multitask_classifier_kwargs)
         featurizer = ('featurizer', featurizer)
@@ -211,7 +215,7 @@ def preset_deepchem_models(trial, data: Dataset) -> list:
     elif model_steps == "robust_multitask_classifier_model":
         featurizer = _get_featurizer(trial, '1D')
         n_features = len(featurizer.feature_names)
-        robust_multitask_classifier_kwargs = {'n_tasks': n_tasks, 'n_features': n_features}
+        robust_multitask_classifier_kwargs = {'n_tasks': n_tasks, 'n_features': n_features, 'n_classes': n_classes}
         model_step = robust_multitask_classifier_model_steps(trial=trial,
                                                              robust_multitask_classifier_kwargs=robust_multitask_classifier_kwargs)
         featurizer = ('featurizer', featurizer)
@@ -219,24 +223,24 @@ def preset_deepchem_models(trial, data: Dataset) -> list:
     elif model_steps == "sc_score_model":
         featurizer = _get_featurizer(trial, '1D')
         n_features = len(featurizer.feature_names)
-        sc_score_kwargs = {'n_features': n_features}
+        sc_score_kwargs = {'n_features': n_features, 'n_classes': n_classes}
         model_step = sc_score_model_steps(trial=trial, sc_score_kwargs=sc_score_kwargs)
         featurizer = ('featurizer', featurizer)
         final_steps.extend([featurizer, model_step[0]])
     elif model_steps == "chem_ception_model":
-        chem_ception_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        chem_ception_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_chem_ception = chem_ception_model_steps(trial=trial, chem_ception_kwargs=chem_ception_kwargs)
         final_steps.extend(steps_chem_ception)
     elif model_steps == "dag_model":
-        dag_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        dag_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_dag = dag_model_steps(trial=trial, dag_kwargs=dag_kwargs)
         final_steps.extend(steps_dag)
     elif model_steps == "graph_conv_model":
-        graph_conv_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        graph_conv_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_graph_conv = graph_conv_model_steps(trial=trial, graph_conv_kwargs=graph_conv_kwargs)
         final_steps.extend(steps_graph_conv)
     elif model_steps == "smiles_to_vec_model":
-        smiles_to_vec_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        smiles_to_vec_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         dataset_copy = deepcopy(data)
         ssf = SmilesSeqFeat()
         ssf.fit_transform(dataset_copy)
@@ -256,12 +260,11 @@ def preset_deepchem_models(trial, data: Dataset) -> list:
         padder = DatasetTransformer(prepare_dataset_for_textcnn, max_length=max_length)
         final_steps.extend([('padder', padder), text_cnn_model_steps(trial=trial, text_cnn_kwargs=text_cnn_kwargs)[0]])
     elif model_steps == "weave_model":
-        weave_kwargs = {'n_tasks': n_tasks, 'mode': mode}
+        weave_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_weave = weave_model_steps(trial=trial, weave_kwargs=weave_kwargs)
         final_steps.extend(steps_weave)
     elif model_steps == "dmpnn_model":
-        # TODO: add support for different n_classes
-        dmpnn_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': 2}
+        dmpnn_kwargs = {'n_tasks': n_tasks, 'mode': mode, 'n_classes': n_classes}
         steps_dmpnn = dmpnn_model_steps(trial=trial, dmpnn_kwargs=dmpnn_kwargs)
         final_steps.extend(steps_dmpnn)
     elif model_steps == "progressive_multitask_regressor_model":
@@ -339,6 +342,7 @@ def preset_keras_models(trial, data: Dataset) -> list:
         List of steps for keras models for hyperparameter optimization.
     """
     mode = data.mode
+    n_classes = len(set(data.y)) if mode == 'classification' else 1
     featurizer_type = trial.suggest_categorical('featurizer_type', ['1D', '2D'])
     featurizer = _get_featurizer(trial, featurizer_type)
     if featurizer_type == '1D':
@@ -352,7 +356,7 @@ def preset_keras_models(trial, data: Dataset) -> list:
         scaler = PassThroughTransformer()
         input_shape = featurizer.fit(data).shape
     # TODO: adjust input size for one hot encoding etc
-    keras_model = _get_keras_model(trial, task_type=mode, featurizer_type=featurizer_type,
+    keras_model = _get_keras_model(trial, task_type=mode, n_classes=n_classes, featurizer_type=featurizer_type,
                                    input_shape=input_shape)
     final_steps = [('standardizer', _get_standardizer(trial)), ('featurizer', featurizer), ('scaler', scaler),
                    ('model', keras_model)]

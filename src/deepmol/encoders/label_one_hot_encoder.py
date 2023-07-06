681ed28bb4076d7ea018551e887611554991c51d
João Correia <jfscorreia95@gmail.com>
2023-07-06 16:34:46 +0100

    [ADD] docstrings

--- src/deepmol/encoders/label_one_hot_encoder.py
+++ src/deepmol/encoders/label_one_hot_encoder.py
@@ -5,12 +5,37 @@ from deepmol.datasets import Dataset
 
 
 class LabelOneHotEncoder(Transformer):
+    """
+    Class that encodes labels as one-hot vectors.
+    This class is used to encode labels as one-hot vectors. This is useful for classification tasks.
+
+    Attributes
+    ----------
+    encoder: OneHotEncoder
+        Scikit-learn one-hot encoder.
+    """
 
     def __init__(self):
+        """
+        Initialize this label encoder.
+        """
         super().__init__()
         self.encoder = OneHotEncoder()
 
     def _fit(self, dataset: Dataset) -> 'LabelOneHotEncoder':
+        """
+        Fit this label encoder.
+
+        Parameters
+        ----------
+        dataset: Dataset
+            Dataset to fit on.
+
+        Returns
+        -------
+        LabelOneHotEncoder
+            Fitted label encoder.
+        """
         # if values are integers, convert them to strings
         y = dataset.y.astype(str) if dataset.y.dtype == int else dataset.y
         # reshape if single feature
@@ -19,6 +44,19 @@ class LabelOneHotEncoder(Transformer):
         return self
 
     def _transform(self, dataset: Dataset) -> Dataset:
+        """
+        Transform the labels of a dataset.
+
+        Parameters
+        ----------
+        dataset: Dataset
+            Dataset to transform.
+
+        Returns
+        -------
+        Dataset
+            Transformed dataset.
+        """
         # if values are integers, convert them to strings
         y = dataset.y.astype(str) if dataset.y.dtype == int else dataset.y
         # reshape if single feature

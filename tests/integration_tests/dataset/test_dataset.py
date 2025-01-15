9d5e6aba168bb0754b459874dce8194f4a3ad96a
jcapels <jcapels96@gmail.com>
2025-01-15 13:47:41 +0000

    [FIX] tests + bump version

--- tests/integration_tests/dataset/test_dataset.py
+++ tests/integration_tests/dataset/test_dataset.py
@@ -23,3 +23,15 @@ class TestDataset(TestCase):
                            labels_fields=['Class'])
 
         self.small_dataset_to_test_with_invalid = loader.create_dataset(sep=",")
+
+        multilabel_classification_df = os.path.join(TEST_DIR, 'data', "multilabel_classification_dataset.csv")
+        loader = CSVLoader(dataset_path=multilabel_classification_df,
+                           smiles_field='smiles',
+                           id_field='ids',
+                           labels_fields=['C00341', 'C01789', 'C00078', 'C00049', 'C00183', 'C03506', 'C00187',
+                                          'C00079', 'C00047', 'C01852', 'C00407', 'C00129', 'C00235', 'C00062',
+                                          'C00353', 'C00148', 'C00073', 'C00108', 'C00123', 'C00135', 'C00448',
+                                          'C00082', 'C00041'],
+                           mode='auto')
+        # create the dataset
+        self.multilabel_classification = loader.create_dataset(sep=',', header=0, nrows=100)
